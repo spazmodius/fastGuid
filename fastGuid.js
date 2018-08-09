21fd54f3ecfae5f4d28aa8871c0b2cef996dd803
rajiv <rajiv.delwadia@versionone.com>
2018-08-09 15:17:21 -0400

    es6-ify

--- fastGuid.js
+++ fastGuid.js
@@ -1,23 +1,22 @@
 
-var hex = (function() {
-	var hex = []
-	for (var i = 0; i < 256; ++i) {
-		hex[i] = (i > 0xF ? '' : '0') + i.toString(16)
-	}
-	return hex
-})()
+const hex = new Array(256)
+for (let i = 0xff; i >= 0x10; --i)
+	hex[i] = i.toString(16)
+for (let i = 0xf; i >= 0; --i)
+	hex[i] = '0' + i.toString(16)
 
-var random_15 = require('crypto').randomBytes.bind(null, 15)
+const crypto = require('crypto')
+const random_15 = () => crypto.randomBytes(15)
 
 function v4_15() {
-	var buf = random_15()
+	const buf = random_15()
 	buf[6] = (buf[6] & 0x0F) | 0x40
 	buf[8] = (buf[8] & 0x3F) | 0x80
 	return buf
 }
 
 function generator(sep, braces) {
-	var leftBrace = braces && braces.charAt(0) || '' , rightBrace = braces && braces.charAt(1) || ''
+	const leftBrace = braces && braces.charAt(0) || '' , rightBrace = braces && braces.charAt(1) || ''
 
 	function format(buf) {
 		return  leftBrace + hex[buf[0]] + hex[buf[1]] + hex[buf[2]] + hex[buf[3]] + sep +
@@ -27,7 +26,7 @@ function generator(sep, braces) {
 			hex[buf[10]] + hex[buf[11]] + hex[buf[12]] + hex[buf[13]] + hex[buf[14]]
 	}
 
-	var formatted15, i = 255
+	let formatted15, i = 255
 	return function() {
 		if (++i >= 256) {
 			formatted15 = format( v4_15() )
@@ -38,7 +37,7 @@ function generator(sep, braces) {
 }
 
 function fastGuid(formatSpecifier) {
-	var generate = fastGuid[formatSpecifier || 'D']
+	const generate = fastGuid[formatSpecifier || 'D']
 	if (!generate)
 		throw new RangeError("Invalid format specifier")
 	return generate()
