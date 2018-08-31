03267deea6019469db7e7f466c02fc21a2ce8fd2
rajiv <rajiv.delwadia@versionone.com>
2018-08-31 17:37:44 -0400

    better benchmarking

--- perftest.js
+++ perftest.js
@@ -1,19 +1,16 @@
+'use strict'
+const Benchmark = require('@spazmodius/hrbench')
+const uuid = require('uuid')
+const fastguid = require('./fastGuid')
 
-const uuid_v4 = require('uuid').v4
-const uuid = () => uuid_v4()
-
-const _fastguid = require('./fastGuid')
-
-const fastguid = () => _fastguid()
-const fastguid_D = () => _fastguid.D()
-const fastguid_N = () => _fastguid.N()
-const fastguid_B = () => _fastguid.B()
-
-exports.compare = {
-	uuid,
-	fastguid,
-	fastguid_D,
-	fastguid_N,
-	fastguid_B,
-}
-require("bench").runMain()
+new Benchmark()
+	.test('uuid', uuid)
+	.test('uuid.v4', uuid.v4)
+	.test(fastguid)
+	.test('fastguid.D', fastguid.D)
+	.test('fastguid.N', fastguid.N)
+	.test('fastguid.B', fastguid.B)
+	.run()
+	.then(Benchmark.summarize)
+	.then(console.log)
+	.catch(console.err)
